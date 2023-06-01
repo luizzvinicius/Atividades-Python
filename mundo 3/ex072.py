44ecc4d8c7a14549d98e0f2d5911104f8edc8ec3
Luiz Vinícius <viniciusadr244@gmail.com>
2023-06-01 09:52:10 -0300

    tuplas done

---
+++ mundo 3/ex072.py
@@ -0,0 +1,6 @@
+import random
+t = (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10))
+
+print(t)
+print(f"Maior = {max(t)}")
+print(f"Menor = {min(t)}")
