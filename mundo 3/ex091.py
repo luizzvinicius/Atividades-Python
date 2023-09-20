7c324a8e21bea8fe3f8ce5833ab575805ce7a776
Luiz Vinícius <93850693+luizzvinicius@users.noreply.github.com>
2023-09-20 11:36:44 -0300

    outra forma de ordenar

--- mundo 3/ex091.py
+++ mundo 3/ex091.py
@@ -6,3 +6,7 @@ for i in range(1, 5):
 
 for i in sorted(info, key=info.get, reverse=True):
     print(i, info[i])
+
+# version 2
+for i in sorted(info.items(), key=lambda pt: pt[1], reverse=True):
+    print(i[0], i[1])
