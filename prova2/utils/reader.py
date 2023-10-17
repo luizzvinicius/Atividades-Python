74a0b8145c63a215ca12680658a6bb07368dd33e
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-17 08:40:49 -0300

    up

---
+++ prova2/utils/reader.py
@@ -0,0 +1,57 @@
+'''Módulo com funções de leitura do teclado'''
+
+'''
+Archives Manipulations Modes
+'r' open for reading (default)
+'w' open for writing, truncating the file first
+'x' open for exclusive creation, failing if the file already exists
+'a' open for writing, appending to the end of file if it exists
+'b' binary mode
+'t' text mode (default)
+'+' open for updating (reading and writing)
+'''
+
+def read_string(msg, exept_msg="Inválido"):
+    '''Função que lê uma string e a retorna já sem os espaços em branco.'''
+    while True:
+        palavra = input(msg).strip()
+        if palavra.isidentifier():
+            return palavra
+        print(exept_msg + "\n")
+
+
+def read_int(msg, exept_msg="Inválido"):
+    '''Função que lê um inteiro e o retorna.'''
+    while True:
+        try:
+            num = int(input(msg))
+            return num
+        except ValueError:
+            print(exept_msg + "\n")
+
+
+def read_float(msg, exept_msg="Inválido"):
+    '''Função que lê um inteiro e o retorna.'''
+    while True:
+        try:
+            num = float(input(msg))
+            return num
+        except ValueError:
+            print(exept_msg + "\n")
+
+
+def read_option(msg, max_opt, min_opt = 0, exept_msg="Opção inválida."):
+    '''Função que lê opções, dado o parâmetro max_opt, e a retorna.'''
+    while True:
+        opt = read_int(msg, "Digite números")
+        if min_opt < opt <= max_opt:
+            return opt
+        print(f"\033[31m{exept_msg}\033[m\n")
+
+
+def fileExists(name):
+    try:
+        with open(name, "r", encoding="utf-8"):
+            return True
+    except (FileNotFoundError, IOError):
+        return False
