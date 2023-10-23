da0a9139378205d8504eb8f03c5f07609d6fabbc
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-23 20:52:24 -0300

    update reader

--- prova2/reader.py
+++ prova2/reader.py
@@ -1,15 +1,5 @@
 '''Módulo com funções de leitura do teclado'''
 
-'''
-Archives Manipulations Modes
-'r' open for reading (default)
-'w' open for writing, truncating the file first
-'x' open for exclusive creation, failing if the file already exists
-'a' open for writing, appending to the end of file if it exists
-'b' binary mode
-'t' text mode (default)
-'+' open for updating (reading and writing)
-'''
 
 def read_string(msg, exept_msg="Inválido"):
     '''Função que lê uma string e a retorna já sem os espaços em branco.'''
@@ -17,7 +7,7 @@ def read_string(msg, exept_msg="Inválido"):
         palavra = input(msg).strip()
         if palavra.isidentifier():
             return palavra
-        print(exept_msg + "\n")
+        print(f"{exept_msg}\n")
 
 
 def read_int(msg, exept_msg="Inválido"):
@@ -27,7 +17,7 @@ def read_int(msg, exept_msg="Inválido"):
             num = int(input(msg))
             return num
         except ValueError:
-            print(exept_msg + "\n")
+            print(f"{exept_msg}\n")
 
 
 def read_float(msg, exept_msg="Inválido"):
@@ -37,21 +27,13 @@ def read_float(msg, exept_msg="Inválido"):
             num = float(input(msg))
             return num
         except ValueError:
-            print(exept_msg + "\n")
+            print(f"{exept_msg}\n")
 
 
-def read_option(msg, max_opt, min_opt = 0, exept_msg="Opção inválida."):
-    '''Função que lê opções, dado o parâmetro max_opt, e a retorna.'''
+def read_option(msg, min_opt, max_opt, exept_msg="Opção inválida."):
+    '''Função que lê opções, dado o parâmetro min_opt e max_opt, e a retorna.'''
     while True:
         opt = read_int(msg, "Digite números")
-        if min_opt < opt <= max_opt:
+        if min_opt <= opt <= max_opt:
             return opt
-        print(f"\033[31m{exept_msg}\033[m\n")
-
-
-def fileExists(name):
-    try:
-        with open(name, "r", encoding="utf-8"):
-            return True
-    except (FileNotFoundError, IOError):
-        return False
+        print(f"{exept_msg}\n")
