c4edb939ac2e4dedfffc68a8d94d8ffed1cac049
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-31 09:06:25 -0300

    refactor

--- prova2/questao1.py
+++ prova2/questao1.py
@@ -1,14 +1,16 @@
 # Nome: Luiz Vinícius Márdelle Costa Silva
-import reader
 from datetime import date
+import util
 
-livros = []
-livros_momento = []
+funcoes = ["Adicionar livro", "Consultar livro", "Adicionar leitura do momento", "Remover leitura momento", "Sair"]
+livros, livros_momento = [], []
 generos = ["Autobiografia", "Biografia", "Conto", "Poesia", "Romance", "Autoajuda"]
 
 def adicionar_livro(titulo, ano_lancamento, autor, genero):
     global livros
     livros.append([titulo, ano_lancamento, autor, genero])
+    print(f"{titulo} adicionado com sucesso\n")
+
 
 def consultar_por_titulo(titulo):
     global livros
@@ -18,6 +20,7 @@ def consultar_por_titulo(titulo):
             livro_mesmo_titulo.append(livro)
     return livro_mesmo_titulo
 
+
 def consultar_por_genero(genero):
     global livros
     livro_mesmo_genero = []
@@ -26,103 +29,85 @@ def consultar_por_genero(genero):
             livro_mesmo_genero.append(livro)
     return livro_mesmo_genero
 
+
 def adicionar_livro_para_leitura(titulo):
     global livros_momento
     livros_momento.append(titulo)
+    print(f"{titulo} adicionado em leitura do momento\n")
+
+
+def livro_ja_cadastrado(titulo, momento=False):
+    global livros, livros_momento
+    if momento:
+        for v in livros_momento:
+            if v == titulo:
+                print("Livro já adicionado\n")
+                return True
+    else:
+        for i, v in enumerate(livros):
+            if livros[i][0] == titulo:
+                print("Livro já adicionado\n")
+                return True
+
+
+def possui_livros(biblioteca):
+    if len(biblioteca) == 0:
+        print("Nenhum livro cadastrado\n")
+    return len(biblioteca) > 0
+
 
 def remover_livro(titulo):
     global livros_momento
     try:
         livros_momento.remove(titulo)
-        print(f"{titulo} removido")
+        print(f"{titulo} removido\n")
     except ValueError:
         print(f"{titulo} não encontrado\n")
 
-funcoes = ["Adicionar livro", "Consultar livro", "Adicionar leitura do momento", "Remover leitura momento", "Sair"]
 
 while True:
-    for i, funcao in enumerate(funcoes, start=1):
-        print(f"[ {i} ] {funcao}")
-
-    opt = reader.read_option("Digite a opção: ", 1, len(funcoes)) - 1
+    util.show_options(funcoes)
+    opt = util.read_option("Digite a opção: ", 1, len(funcoes)) - 1
     if opt == len(funcoes) - 1:
         print("Programa encerrado.")
         break
-
     if opt == 0:
-        titulo = reader.read_string("Digite o título do livro: ", "Digite apenas letras (sem espaços) para o título do livro").lower()
-        for livro in livros:
-            if titulo == livro[0]:
-                print("Livro já cadastrado")
-                continue
-        ano_lancamento = reader.read_option("Digite o ano de lançamento do livro: ", 1900, date.today().year, f"Ano deve ser após 1900 e antes de {date.today().year}")
-        autor = reader.read_string(f"Digite o nome do autor de {titulo}: ", "Digite apenas letras (sem espaços) para o autor do livro")
-        for i, genero in enumerate(generos, start=1):
-            print(f"[ {i} ] {genero}")
-        opt_genero = reader.read_option(f"Selecione o gênero de {titulo}: ", 1, len(generos), "Gênero inválido") - 1
+        titulo = util.read_string("Digite o título do livro: ", "Digite apenas letras (sem espaços) para o título do livro")
+        if livro_ja_cadastrado(titulo): continue
+        ano_lancamento = util.read_option("Digite o ano de lançamento: ", 1900, date.today().year, f"Ano deve ser após 1900 e antes de {date.today().year}")
+        autor = util.read_string(f"Digite o nome do autor de {titulo}: ", "Digite apenas letras (sem espaços) para o autor do livro")
+        util.show_options(generos)
+        opt_genero = util.read_option(f"Selecione o gênero de {titulo}: ", 1, len(generos), "Gênero inválido") - 1
         adicionar_livro(titulo, ano_lancamento, autor, generos[opt_genero])
-        print(f"{titulo} adicionado com sucesso\n")
     elif opt == 1:
-        if len(livros) == 0:
-            print("Nenhum livro cadastrado\n")
-            continue
-        
-        print("[ 1 ] Por título\n[ 2 ] Por gênero")
-        forma_consulta = reader.read_option("Qual a forma de consulta? ", 1, 2, "Forma de consulta inválida") - 1
+        if not possui_livros(livros): continue
+        forma_consulta = util.read_option("[ 1 ] Por título\n[ 2 ] Por gênero\nQual a forma de consulta? ", 1, 2, "Consulta inválida") - 1
         if forma_consulta == 0:
-            titulo = reader.read_string("Digite o título do livro: ", "Digite apenas letras (sem espaços) para o título do livro")
+            titulo = util.read_string("Digite o título do livro: ", "Digite apenas letras (sem espaços) para o título do livro")
             resultado = consultar_por_titulo(titulo)
-            if len(resultado) == 0:
-                print("Livro(s) não encontrado(s)\n")
-                continue
-            
-            for livro in resultado:
-                print(f"Título: {livro[0]}")
-                print(f"Ano lançamento: {livro[1]}")
-                print(f"Autor: {livro[2]}")
-                print(f"Gênero: {livro[3]}")
-                print()
         elif forma_consulta == 1:
-            for i, genero in enumerate(generos, start=1):
-                print(f"[ {i} ] {genero}")
-            opt_genero = reader.read_option(f"Selecione o gênero de {titulo}: ", 1, len(generos), "Gênero inválido") - 1
-            
+            util.show_options(generos)
+            opt_genero = util.read_option("Selecione o gênero: ", 1, len(generos), "Gênero inválido") - 1
             resultado = consultar_por_genero(generos[opt_genero])
-            if len(resultado) == 0:
-                print("Nenhum livro com esse gênero foi encontrado\n")
-                continue
-            
-            for livro in resultado:
-                print(f"Título: {livro[0]}")
-                print(f"Ano lançamento: {livro[1]}")
-                print(f"Autor: {livro[2]}")
-                print(f"Gênero: {livro[3]}")
-                print()
+        if not possui_livros(resultado): continue
+        for livro in resultado:
+            print(f"Título: {livro[0]}")
+            print(f"Ano lançamento: {livro[1]}")
+            print(f"Autor: {livro[2]}")
+            print(f"Gênero: {livro[3]}")
+            print()
     elif opt == 2:
-        if len(livros) == 0:
-            print("Nenhum livro sendo lido atualmente.")
-            continue
+        if not possui_livros(livros): continue
         if len(livros_momento) == 3:
-            print("Quantidade máxima de livros atingida (3)")
+            print("Quantidade máxima de livros atingida (3)\n")
             continue
-
         for i, livro in enumerate(livros, start=1):
             print(f"[ {i} ] {livro[0]}")
-        livro_escolhido = reader.read_option("Selecione o livro que você está lendo atualmente: ", 1, len(livros)) - 1
-        ja_adicionado = False
-        for i, titulo in enumerate(livros_momento):
-            if livros[livro_escolhido][0] == titulo:
-                ja_adicionado = True
-                print("Livro já adicionado")
-        if ja_adicionado: continue
-        print(f"{livros[livro_escolhido][0]} adicionado a leitura do momento\n")
+        livro_escolhido = util.read_option("Selecione o livro que você está lendo atualmente: ", 1, len(livros)) - 1
+        if livro_ja_cadastrado(livros[livro_escolhido][0], True): continue
         adicionar_livro_para_leitura(livros[livro_escolhido][0])
     elif opt == 3:
-        if len(livros_momento) == 0:
-            print("Nenhum livro sendo lido atualmente\n")
-            continue
-
-        for i, titulo in enumerate(livros_momento, start=1):
-            print(f"{i} {titulo}")
-        livro_removido = reader.read_option("Selecione o livro a ser removido: ", 1, len(livros_momento), "Digite apenas letras (sem espaços) para o título do livro") - 1
+        if not possui_livros(livros_momento): continue
+        util.show_options(livros_momento)
+        livro_removido = util.read_option("Selecione o livro a ser removido: ", 1, len(livros_momento), "Digite apenas letras (sem espaços) para o título do livro") - 1
         remover_livro(livros_momento[livro_removido])
--- prova2/util.py
+++ prova2/util.py
@@ -4,7 +4,7 @@
 def read_string(msg, exept_msg="Inválido"):
     '''Função que lê uma string e a retorna já sem os espaços em branco.'''
     while True:
-        palavra = input(msg).strip()
+        palavra = input(msg).strip().lower()
         if palavra.isidentifier():
             return palavra
         print(f"{exept_msg}\n")
@@ -20,16 +20,6 @@ def read_int(msg, exept_msg="Inválido"):
             print(f"{exept_msg}\n")
 
 
-def read_float(msg, exept_msg="Inválido"):
-    '''Função que lê um inteiro e o retorna.'''
-    while True:
-        try:
-            num = float(input(msg))
-            return num
-        except ValueError:
-            print(f"{exept_msg}\n")
-
-
 def read_option(msg, min_opt, max_opt, exept_msg="Opção inválida."):
     '''Função que lê opções, dado o parâmetro min_opt e max_opt, e a retorna.'''
     while True:
@@ -37,3 +27,8 @@ def read_option(msg, min_opt, max_opt, exept_msg="Opção inválida."):
         if min_opt <= opt <= max_opt:
             return opt
         print(f"{exept_msg}\n")
+
+
+def show_options(array):
+    for i, funcao in enumerate(array, start=1):
+        print(f"[ {i} ] {funcao}")
